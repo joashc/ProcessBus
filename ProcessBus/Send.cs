f3f0f7f9d349a634f1780d0bbcf561e1fec6db50
joashc <joash.is@gmail.com>
2016-03-08 22:43:55 +1100

    Initial commit

---
+++ ProcessBus/Send.cs
@@ -0,0 +1,18 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LanguageExt.Process;
+
+namespace ProcessBus
+{
+    public static class Send
+    {
+        public static Unit send<T>(string transport, T message)
+        {
+            return Unit.Default;
+        }
+    }
+}
